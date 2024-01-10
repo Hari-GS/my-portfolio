fd249e161c01380c93db46d9c66a122149603d2a
Hari G <sakthihariharan007@gmail.com>
2024-01-10 13:09:37 +0530

    i added minor projects card

---
+++ my-app/src/Components/MinorProjects.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./MinorProjectsStyles.css"
+import p1 from '../assets/cal project.png'
+import p2 from '../assets/console emp app.webp'
+import p3 from '../assets/rest api image.jpg'
+
+function MinorProjects() {
+    return (
+        <div>
+        <h1 className="heading">Minor Projects</h1>
+        <div className="container">
+            
+            <div className="pro-1">
+                <img className="image" src={p1} alt="Sci cal"/>
+                <h1 className="min-head">Basic Calculator</h1>
+                <p className="para">Tech Stack : Java swings</p>
+            </div>
+            <div className="pro-1">
+                <img className="image" src={p2} alt="Sci cal"/>
+                <h1 className="min-head">Simple Web API</h1>
+                <p className="para">Tech Stack : Java Spring Boot | REST API</p>
+            </div>
+            <div className="pro-1">
+                <img className="image" src={p3} alt="Sci cal"/>
+                <h1 className="min-head">Console based employment management system</h1>
+                <p className="para">Tech Stack : C Programming</p>
+            </div>        
+        </div>
+        </div>
+        
+    )
+}
+
+export default MinorProjects
--- my-app/src/Routes/Projects.js
+++ my-app/src/Routes/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import ProjectData from '../Components/ProjectData'
+import MinorProjects from '../Components/MinorProjects'
 
 function Projects() {
     return (
         <div>
             <Navbar/>
             <ProjectData/>
+            <MinorProjects/>
         </div>
     )
 }
