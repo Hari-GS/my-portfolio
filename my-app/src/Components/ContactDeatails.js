6bb66a1545d70195fff623288ab39e73218ec7f7
Hari G <sakthihariharan007@gmail.com>
2024-02-02 22:07:09 +0530

    bg of nav options is removed

--- my-app/src/Components/ContactDeatails.js
+++ my-app/src/Components/ContactDeatails.js
@@ -4,7 +4,7 @@ import "./ContactDeatails.css"
 function ContactDeatails() {
     return (
         <div className="details-list">
-            <h3>Email : <a href="mailto:sakthihariharan007@gmail.com">sakthihariharan007@gmail.com</a></h3>
+            <h3>Email : <a href="mailto:hariharandev02@gmail.com">hariharandev02@gmail.com</a></h3>
             <h3>LinkedIn : <a href="https://www.linkedin.com/in/dev-hariharan-g" target="_blank">www.linkedin.com/in/dev-hariharan-g</a></h3>
             <h3>GitHub : <a href="https://github.com/Hari-GS" target="_blank">https://github.com/Hari-GS</a></h3>
         </div>
